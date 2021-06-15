8e72373177a931c0467224605df57bffc3cf2561
Vigneshrajvr <85781194+Vigneshrajvr@users.noreply.github.com>
2021-06-15 19:54:58 +0530

    Add files via upload

---
+++ day4task.py
@@ -0,0 +1,19 @@
+a=b=c=80;
+print(a/10);
+print(b*50);
+print(c+10);
+var='vicky';
+print(var.replace('c','G'));
+a=8;
+b=4.6
+print('Before coversion');
+print('a=',a);
+print('b=',b);
+a=float(a);
+b=int(b);
+print('After coversion');
+print('a=',a);
+print(type(a));
+print('b=',b);
+print(type(b));
+
