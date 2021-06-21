730b9b270fd5cdd520e9bc5ed3f7e0e189c3ab3b
Vigneshrajvr <85781194+Vigneshrajvr@users.noreply.github.com>
2021-06-21 13:48:15 +0530

    Add files via upload

--- day8task.py
+++ day8task.py
@@ -1,39 +1,39 @@
 print('1.Python script to merge two Python dictionaries')
-dc={1:'supes',2:'bat',3:'flash'}
-marv={4:'cap',5:'thor'}
-print('Dc : ',dc)
-print('Marvel : ',marv)
-dc.update(marv)
-print('Merged Dictonary Heroes: ',dc)
-print('2.Program to sort the value from descending to ascending in list and convert it into a set')
-a=[5,4,3,2,1]
-print('list : ',a)
-a.sort()
-print('sorted list : ',a)
-seta=set(a)
-print('list after converting to set : ',seta)
+dict1={1:'one',2:'two',3:'three'}
+dict2={4:'four',5:'five'}
+print('Dict1 : ',dict1)
+print('Dict2 : ',dict2)
+dict1.update(dict2)
+print('Merged Dictonary : ',dict1)
+print('2.program to sort the value from descending to ascending in list and convert it in to a set')
+list1=[5,4,3,2,1]
+print('list1 : ',list1)
+list1.sort()
+print('sorted list1 : ',list1)
+set1=set(list1)
+print('list1 after converting to set : ',set1)
 print('3.Python program to list number of items in a dictionary key and sort the list with the help of a function & without the function')
-dict={'key':[9,8,7,6,5]}
-print('dict : ',dict)
-dict['key'].sort()
-print('dict after sorting its items with function : ',dict)
-dict1={'key':[5,4,3,2,1]}
+dict3={'key':[5,4,3,2,1]}
+print('dict3 : ',dict3)
+dict3['key'].sort()
+print('dict3 after sorting its items with function : ',dict3)
+dict4={'key':[5,4,3,2,1]}
 print('sorting the items without using the function')
-for i in range(len(dict1['key'])):
-    for j in range(i + 1, len(dict1['key'])):
-        if dict1['key'][i] > dict1['key'][j]:
-           dict1['key'][i], dict1['key'][j] = dict1['key'][j], dict1['key'][i]
-print('dict1 after sorting its items without function : ',dict1)
+for i in range(len(dict4['key'])):
+    for j in range(i + 1, len(dict4['key'])):
+        if dict4['key'][i] > dict4['key'][j]:
+           dict4['key'][i], dict4['key'][j] = dict4['key'][j], dict4['key'][i]
+print('dict4 after sorting its items without function : ',dict4)
 print('4.Python program to get a string from a given string (user input) and change the first occurrence of the word to a user specified input.')
-str='countryname is my country'
-print('string : ',str)
-str1=input('Enter your country name : ')
-str2=str.replace('countryname', str1)
-print('After changing the first occurance of the string with user specified input : ',str2)
-print('5.Python program to get a string from a given string where all occurrences of its first char have been changed to capital letter')
-string='the red car is nice and the car looks like bmw car'
-print('string : ',string)
-list2=string.split()
+str1='what is my name'
+print('str1 : ',str1)
+str2=input('Enter your  name : ')
+str3=str1.replace('what', str2)
+print('After changing the first occurance of the string with user specified input : ',str3)
+print('5.Write a Python program to get a string from a given string where all occurrences of its first char have been changed to capital letter')
+str4='hello there general kenobi, hello  !'
+print('str4 : ',str4)
+list2=str4.split()
 str5=input('Enter the word that you want to capitalize : ')
 str6=''
 for i in list2:
@@ -43,7 +43,7 @@ for i in list2:
     str6+=(i+' ')
 print('After changing all the occurances of the word in str4 :',str6)
 print('6.Python program to find the repeated items of a list')
-list3=[1,1,3,2,1,9,0,3,1,1,2,6]
+list3=[1,1,3,6,3,9,0,3,1,9,2,6]
 print('list3 : ',list3)
 print('The repeated items are : ',end='')
 list4=[]
@@ -53,9 +53,9 @@ for i in range(len(list3)):
         print(list3[i],end=' ')
 print()
 print('7.Python program to check the sum of three elements and divided by a value which is given as an input by the user')
-num1=int(input('Enter the number 1 : '))
-num2=int(input('Enter the number 2 : '))
-num3=int(input('Enter the number 3 : '))
+num1=int(input('Enter  number 1 : '))
+num2=int(input('Enter  number 2 : '))
+num3=int(input('Enter  number 3 : '))
 val=int(input('Enter the divisor value : '))
 print('After performning the operation : ',(num1+num2+num3)/val)
 print('8.Python program to find the Mean,median,mode among three given numbers')
@@ -69,11 +69,11 @@ for i in range(len(list5)):
         print('Mode : ',list5[i])
         break
 print('9.Python program to swap cases of a given string')
-str7='Python Is My Favourite Language'
+str7='This Is The Way'
 print('str7 : ',str7)
 print('After swapping the cases : ',str7.swapcase())
 print('10.program to convert an integer to binary & octa decimal')
-num4=16
-print('num6 : ',num4)
-print('num6 in binary : ',bin(num4))
-print('num6 in octa decimal : ',oct(num4))
+num=32
+print('num6 : ',num)
+print('num6 in binary : ',bin(num))
+print('num6 in octa decimal : ',oct(num))
