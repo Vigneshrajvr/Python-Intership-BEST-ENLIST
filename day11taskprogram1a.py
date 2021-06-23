f8d76a93da214fecf39a4839becb84d829b6a7f5
Vigneshrajvr <85781194+Vigneshrajvr@users.noreply.github.com>
2021-06-23 22:14:29 +0530

    Add files via upload

---
+++ day11taskprogram1a.py
@@ -0,0 +1,6 @@
+import day11taskprogram1 as var
+print('List present in the module day11taskprogram1 : ',var.list)
+
+var.list[3]=0
+
+print('After changing the 4rd value in the list present in the module : ',var.list)
---
+++ day11taskprogram2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+dict = {
+    'Games': ['GodOfWar','AssasinsCreed','DaysGone'],
+    'price':[1000,1500,1300]
+}
+
+var = pd.DataFrame(dict)
+
+print(var)
