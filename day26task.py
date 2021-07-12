9ee859d2d51833cc968e19179922c87fdc277886
Vigneshrajvr <85781194+Vigneshrajvr@users.noreply.github.com>
2021-07-12 21:20:30 +0530

    Add files via upload

---
+++ day26task.py
@@ -0,0 +1,5 @@
+list1= ['ruby','bala','goku','vicky','shree']
+print(list(enumerate(list1)))
+print(enumerate(list1))
+tuple1=('rubesh','balaji','gokul','vignesh','shreenath')
+print(tuple(enumerate(tuple1)))
